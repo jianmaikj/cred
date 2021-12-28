7f99134f486c5853ff5b43759d24abd451169cc5
jianmaikj <75167751+jianmaikj@users.noreply.github.com>
2021-12-28 16:43:47 +0800

    feat：add generate RSA keys

--- rsa.go
+++ rsa.go
@@ -6,11 +6,17 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"log"
+	"os"
 	"runtime"
 )
 
 type RSACredManager struct{}
-
+const (
+	privateFileName="private.pem"
+	publicFileName="public.pem"
+	privateKeyPrefix="RSA PRIVATE KEY "
+	publicKeyPrefix="RSA PUBLIC KEY "
+)
 func NewRSA() *RSACredManager {
 	cm := &RSACredManager{}
 	return cm
@@ -63,4 +69,40 @@ func (cm *RSACredManager)Decrypt(cryptText ,key []byte)(plainText []byte,err err
 		return []byte{},err
 	}
 	return plainText,nil
+}
+
+func (cm *RSACredManager)GenerateKey()error{
+	privateKey, err:= rsa.GenerateKey(rand.Reader, 2048)
+	if err!=nil{
+		return err
+	}
+	x509PrivateKey := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateFile, err := os.Create(privateFileName)
+	if err!=nil{
+		return err
+	}
+	defer privateFile.Close()
+	privateBlock := pem.Block{
+		Type:privateKeyPrefix,
+		Bytes:x509PrivateKey,
+	}
+
+	if err=pem.Encode(privateFile,&privateBlock);err!=nil{
+		return err
+	}
+	publicKey := privateKey.PublicKey
+	x509PublicKey, err := x509.MarshalPKIXPublicKey(&publicKey)
+	if err != nil {
+		panic(err)
+	}
+	publicFile,_ :=os.Create(publicFileName)
+	defer publicFile.Close()
+	publicBlock := pem.Block{
+		Type:publicKeyPrefix,
+		Bytes:x509PublicKey,
+	}
+	if err=pem.Encode(publicFile,&publicBlock);err!=nil{
+		return err
+	}
+	return nil
 }
